70d5e2acedba2e3132386bd56731d7873c45c4d3
Edelweiss <redwitblue@gmail.com>
2022-11-12 20:06:00 -0600

    updoot

---
+++ orig.rs
@@ -0,0 +1,142 @@
+use std::fs::OpenOptions;
+use std::{fs, io};
+
+use anyhow::{anyhow, Result};
+use chrono::{Duration, Utc};
+use ron::ser::{to_string_pretty, PrettyConfig};
+use serde::{Deserialize, Serialize};
+
+enum ParsedInput {
+    ValidNumber(u64),
+    InvalidInput,
+}
+
+fn is_string_numeric(str: &str) -> bool {
+    !str.chars().any(|c| !c.is_numeric())
+}
+
+struct DurationConverter;
+
+impl DurationConverter {
+    fn to_seconds(value: &str) -> ParsedInput {
+        if is_string_numeric(value) {
+            match value.parse::<u64>() {
+                Ok(n) => ParsedInput::ValidNumber(n),
+                Err(_) => return ParsedInput::InvalidInput,
+            }
+        } else {
+            let multiplier = match value.chars().last().unwrap_or_else(|| 's') {
+                's' => 1,
+                'm' => 60,
+                'h' => 3600,
+                'd' => 86400,
+                'w' => 604800,
+                'M' => 2592000,
+                'y' => 31536000,
+                _ => 0,
+            };
+            let number = value
+                .strip_suffix(|_: char| true)
+                .unwrap_or_else(|| "1")
+                .parse::<u64>();
+
+            match number {
+                Ok(n) => ParsedInput::ValidNumber(n * multiplier),
+                Err(_) => ParsedInput::InvalidInput,
+            }
+        }
+    }
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct Task {
+    title: String,
+    description: String,
+    due: chrono::DateTime<chrono::Utc>,
+}
+
+fn create_task() -> Result<Task> {
+    let mut task_name = String::new();
+    let mut task_desc = String::new();
+    let mut task_due = String::new();
+
+    println!("-------------------------------");
+    println!("Enter the Task's Name: ");
+    io::stdin().read_line(&mut task_name)?;
+    println!("Enter the Task's Description: ");
+    io::stdin().read_line(&mut task_desc)?;
+    println!("When is it due?");
+    println!("GUIDE:[s:Seconds, m:Minutes, h:Hours, d:Days, w:Weeks, M:Months, y:Years]");
+    println!("Example: 30m :: 30 Minutes");
+    io::stdin().read_line(&mut task_due)?;
+    println!("-------------------------------");
+
+    match DurationConverter::to_seconds(&task_due.trim().to_string()) {
+        ParsedInput::ValidNumber(number) => Ok(Task {
+            title: task_name.trim().to_string(),
+            description: task_desc.trim().to_string(),
+            due: chrono::Utc::now() + Duration::seconds(number as i64),
+        }),
+        ParsedInput::InvalidInput => {
+            println!("A valid number was not put in.");
+            Err(anyhow!("Failed to create task, cannot parse input"))
+        }
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    const PATH: &str = "tasks.ron";
+    // Ensure Creation
+    OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&PATH)?;
+
+    let mut data = fs::read_to_string(&PATH)?;
+
+    let mut tasks = ron::from_str::<Vec<Task>>(&data).unwrap_or_else(|_| Vec::new());
+
+    loop {
+        let mut choice = String::new();
+
+        println!("Want to create another task?");
+        io::stdin().read_line(&mut choice)?;
+        match choice.trim().to_lowercase().as_str() {
+            "y" => {
+                match create_task() {
+                    Ok(task) => tasks.push(task),
+                    Err(ex) => {
+                        println!("{}", ex);
+                        continue;
+                    }
+                }
+                continue;
+            }
+            _ => break,
+        }
+    }
+
+    data = to_string_pretty(&tasks, PrettyConfig::new())?;
+    fs::write(&PATH, &data)?;
+
+    let mut tasks = ron::from_str::<Vec<Task>>(&data)?;
+
+    for task in tasks.iter() {
+        println!("----------------------");
+        println!("{:?}", task);
+        println!("......................");
+        match task.due <= Utc::now() {
+            true => {
+                println!("It is time to complete this task.");
+            }
+            false => println!("It is not time to complete this task yet."),
+        }
+    }
+
+    tasks.retain(|t| t.due >= Utc::now());
+
+    data = to_string_pretty(&tasks, PrettyConfig::new())?;
+    fs::write(&PATH, &data)?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,93 @@
-use std::fs::OpenOptions;
-use std::{fs, io};
+use anyhow::Result;
+use std::{
+    io,
+    sync::{Arc, Mutex},
+    thread,
+};
 
-use anyhow::{anyhow, Result};
-use chrono::{Duration, Utc};
-use ron::ser::{to_string_pretty, PrettyConfig};
-use serde::{Deserialize, Serialize};
-
-enum ParsedInput {
-    ValidNumber(u64),
-    InvalidInput,
-}
-
-fn is_string_numeric(str: &str) -> bool {
-    !str.chars().any(|c| !c.is_numeric())
-}
-
-struct DurationConverter;
-
-impl DurationConverter {
-    fn to_seconds(value: &str) -> ParsedInput {
-        if is_string_numeric(value) {
-            match value.parse::<u64>() {
-                Ok(n) => ParsedInput::ValidNumber(n),
-                Err(_) => return ParsedInput::InvalidInput,
-            }
-        } else {
-            let multiplier = match value.chars().last().unwrap_or_else(|| 's') {
-                's' => 1,
-                'm' => 60,
-                'h' => 3600,
-                'd' => 86400,
-                'w' => 604800,
-                'M' => 2592000,
-                'y' => 31536000,
-                _ => 0,
-            };
-            let number = value
-                .strip_suffix(|_: char| true)
-                .unwrap_or_else(|| "1")
-                .parse::<u64>();
-
-            match number {
-                Ok(n) => ParsedInput::ValidNumber(n * multiplier),
-                Err(_) => ParsedInput::InvalidInput,
-            }
-        }
-    }
-}
-#[derive(Debug, Serialize, Deserialize)]
-struct Task {
-    title: String,
-    description: String,
-    due: chrono::DateTime<chrono::Utc>,
-}
-
-fn create_task() -> Result<Task> {
-    let mut task_name = String::new();
-    let mut task_desc = String::new();
-    let mut task_due = String::new();
-
-    println!("-------------------------------");
-    println!("Enter the Task's Name: ");
-    io::stdin().read_line(&mut task_name)?;
-    println!("Enter the Task's Description: ");
-    io::stdin().read_line(&mut task_desc)?;
-    println!("When is it due?");
-    println!("GUIDE:[s:Seconds, m:Minutes, h:Hours, d:Days, w:Weeks, M:Months, y:Years]");
-    println!("Example: 30m :: 30 Minutes");
-    io::stdin().read_line(&mut task_due)?;
-    println!("-------------------------------");
-
-    match DurationConverter::to_seconds(&task_due.trim().to_string()) {
-        ParsedInput::ValidNumber(number) => Ok(Task {
-            title: task_name.trim().to_string(),
-            description: task_desc.trim().to_string(),
-            due: chrono::Utc::now() + Duration::seconds(number as i64),
-        }),
-        ParsedInput::InvalidInput => {
-            println!("A valid number was not put in.");
-            Err(anyhow!("Failed to create task, cannot parse input"))
-        }
-    }
-}
-
-fn main() -> anyhow::Result<()> {
-    const PATH: &str = "tasks.ron";
-    // Ensure Creation
-    OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(&PATH)?;
-
-    let mut data = fs::read_to_string(&PATH)?;
-
-    let mut tasks = ron::from_str::<Vec<Task>>(&data).unwrap_or_else(|_| Vec::new());
-
+fn read_user_input(tasks: Arc<Mutex<Vec<Task>>>) {
     loop {
-        let mut choice = String::new();
+        let mut check = String::new();
+        println!("Want to add a task? [Y/N]: ");
+        io::stdin().read_line(&mut check).unwrap(); // for now
 
-        println!("Want to create another task?");
-        io::stdin().read_line(&mut choice)?;
-        match choice.trim().to_lowercase().as_str() {
-            "y" => {
-                match create_task() {
-                    Ok(task) => tasks.push(task),
-                    Err(ex) => {
-                        println!("{}", ex);
-                        continue;
-                    }
-                }
-                continue;
-            }
-            _ => break,
+        if check.to_lowercase().trim() == "y" {
+            let mut task_name = String::new();
+            let mut task_desc = String::new();
+
+            println!("-------------------------------");
+            println!("Enter the Task's Name: ");
+            io::stdin().read_line(&mut task_name).unwrap(); // for now
+            println!("Enter the Task's Description: ");
+            io::stdin().read_line(&mut task_desc).unwrap(); // for now
+            println!("-------------------------------");
+
+            let new_task = Task::new(task_name, task_desc, 10);
+            tasks.lock().unwrap().append(&mut vec![new_task])
+        } else {
+            break;
         }
     }
+}
 
-    data = to_string_pretty(&tasks, PrettyConfig::new())?;
-    fs::write(&PATH, &data)?;
-
-    let mut tasks = ron::from_str::<Vec<Task>>(&data)?;
-
-    for task in tasks.iter() {
-        println!("----------------------");
-        println!("{:?}", task);
-        println!("......................");
-        match task.due <= Utc::now() {
-            true => {
-                println!("It is time to complete this task.");
-            }
-            false => println!("It is not time to complete this task yet."),
+fn check_tasks(tasks: Arc<Mutex<Vec<Task>>>) {
+    for task in tasks.lock().unwrap().iter() {
+        if task.is_finished {
+            println!("Task \"{:?}\" was completed.", task.name);
         }
     }
+}
 
-    tasks.retain(|t| t.due >= Utc::now());
+fn main() -> Result<()> {
+    let tasks = Arc::new(Mutex::new(vec![]));
 
-    data = to_string_pretty(&tasks, PrettyConfig::new())?;
-    fs::write(&PATH, &data)?;
+    let input_tasks_pointer = tasks.clone();
+    let check_tasks_pointer = tasks.clone();
 
+    let check_tasks_handle = thread::spawn(|| check_tasks(check_tasks_pointer));
+    let read_user_input_handle = thread::spawn(|| read_user_input(input_tasks_pointer));
+    read_user_input_handle.join();
+    check_tasks_handle.join();
     Ok(())
+}
+
+struct Task {
+    name: String,
+    description: String,
+    is_finished: bool,
+    start_time: chrono::DateTime<chrono::Utc>,
+    end_time: chrono::DateTime<chrono::Utc>,
+}
+
+impl Task {
+    /// Creates a new Task.
+    fn new<IntoStr: Into<String>>(name: IntoStr, description: IntoStr, timeout: i64) -> Self {
+        let start_time = chrono::Utc::now();
+        Self {
+            name: name.into(),
+            description: description.into(),
+            is_finished: false,
+            start_time: start_time,
+            end_time: start_time + chrono::Duration::seconds(timeout),
+        }
+    }
+    /// Starts the task.
+    fn start(&mut self) {
+        loop {
+            self.update();
+            if self.is_finished {
+                break;
+            }
+        }
+    }
+
+    /// Updates the task for every duration
+    fn update(&mut self) {
+        if self.is_finished {
+            // Finished Behavior
+        }
+        if self.end_time <= chrono::Utc::now() {
+            self.is_finished = true;
+        }
+    }
 }
